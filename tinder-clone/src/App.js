3fbdbbf38e7405d462549995361f12d6062bd6e5
Mayank <mayankmahajan936@gmail.com>
2023-08-29 22:32:05 +0530

    tinder clone frontend

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import Card from "./Card";
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Tinder cards */}
+      <Card />
+      {/* Swipe Buttons */}
+    </div>
+  );
+}
+
+export default App;
